22f5d96db5eb21936560cf013c60d4fc9dadec1b
troyfreed <96844527+troyfreed@users.noreply.github.com>
2025-02-19 22:41:03 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,5 +6,5 @@ from main import *
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(5), BinaryNumber(0)) == 5 * 0
     assert quadratic_multiply(BinaryNumber(15), BinaryNumber(7)) == 15 * 7
-    assert quadratic_multiply(BinaryNumber(123), BinaryNumber(456)) == 123 * 456,
+    assert quadratic_multiply(BinaryNumber(123), BinaryNumber(456)) == 123 * 456
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
